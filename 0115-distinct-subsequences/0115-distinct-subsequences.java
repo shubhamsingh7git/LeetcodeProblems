f5cd9e2a1445394a191b1dd6d23cf6389c65b934
Shubham Singh <shubham.singh_cs23@gla.ac.in>
2026-09-06 21:50:03 +0530

    Time: 20 ms (53.25%), Space: 66.5 MB (6.38%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -2,13 +2,10 @@ class Solution {
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
-
         long[][] dp = new long[n + 1][m + 1];
-
         for(int i = 0; i <= n; i++){
             dp[i][0] = 1;
         }
-
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 if(s.charAt(i - 1) == t.charAt(j - 1)){
@@ -18,7 +15,6 @@ class Solution {
                 }
             }
         }
-
         return (int)dp[n][m];
     }
 }
